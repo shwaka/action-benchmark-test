f48fbd523e8e2740dbac8588923b38ba1a5b6127
github-action-benchmark <github@users.noreply.github.com>
2021-11-23 10:13:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d055f63d98c78adef3a4de0221f07a2c795a953c

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1637662396709,
+  "repoUrl": "https://github.com/shwaka/action-benchmark-test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "shun.wakatsuki@gmail.com",
+            "name": "Shun Wakatsuki",
+            "username": "shwaka"
+          },
+          "committer": {
+            "email": "shun.wakatsuki@gmail.com",
+            "name": "Shun Wakatsuki",
+            "username": "shwaka"
+          },
+          "distinct": true,
+          "id": "d055f63d98c78adef3a4de0221f07a2c795a953c",
+          "message": "update",
+          "timestamp": "2021-11-23T17:53:46+09:00",
+          "tree_id": "af27c0d7689b6b9ce037679f7275b731d894901a",
+          "url": "https://github.com/shwaka/action-benchmark-test/commit/d055f63d98c78adef3a4de0221f07a2c795a953c"
+        },
+        "date": 1637662396228,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      }
+    ]
+  }
+}
